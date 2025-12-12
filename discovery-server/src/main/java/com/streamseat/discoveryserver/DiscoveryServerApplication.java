8fe440a9db719cce20b7533c16f6932f8b8cdf20
Yash Vardhan Shukla <lakkyshukla74339@gmail.com>
2025-12-12 15:54:47 +0530

    created a binder/folder which take care of all the services and their versions, inside it created discovery-server which is a eureka server and also created event-service in which i added eureka-client dependency so that it gets registered in eureka server

---
+++ discovery-server/src/main/java/com/streamseat/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.streamseat.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // <--- This annotation activates the Registry
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
